9d2f115ff928bdc258bff34bb4e154d972b6b301
2linawr <61121260+2linawr@users.noreply.github.com>
2022-08-18 16:58:40 +0300

    Update mail(php).php

--- mail(php).php
+++ mail(php).php
@@ -1,5 +1,5 @@
 <?php 
-if(empty($_POST['name'])&& empty($_POST['email'])&& empty($_POST['telephone'])){
+//if(empty($_POST['name'])&& empty($_POST['email'])&& empty($_POST['telephone'])){
 $to="doc.docit@mail.ru";
 $from="linar210998@yandex.ru";
 
@@ -8,8 +8,8 @@ $headers = "From:$from"."\r\n".
 "Reply-To:$from"."\r\n";
 mail("doc.docit@mail.ru",$subject,$message,$headers)
 echo'Данные есть';
- }
-else{
-echo'Данных нет';
 }
+//else{
+//echo'Данных нет';
+//}
 ?>
